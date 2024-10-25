6111415ed52e25c2ee4c565d8053f5d94063fe64
Kira <78575847+Syren0914@users.noreply.github.com>
2024-10-25 10:31:58 -0400

    update

---
+++ src/app/page.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+      <main>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Customer</th>
+                <th>Status</th>
+                <th>Type</th>
+                <th>Total</th>
+
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </main>
+    
+  );
+}
